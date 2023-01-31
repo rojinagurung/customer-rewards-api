4fcd3ecc05861aca5f6d7a59d571b6ade08895f5
Rojina Gurung <rojinagurung997@gmail.com>
2023-01-30 21:04:13 -0500

    Spring initializer commit

---
+++ src/test/java/com/retail/rewards/customerrewardsapi/CustomerRewardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.rewards.customerrewardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRewardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
